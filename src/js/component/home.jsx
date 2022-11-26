0d3d20de80b3c2a36cd03c0ca8070f098ab3dc45
INVIiICTUS <j.enriquealorenzo@gmail.com>
2022-11-26 10:12:02 +0000

    cuenta y resetea

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,26 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Contador from "./Contador";
 
 //create your first component
-const Home = () => {
+const Home = ({
+	segundos,
+	minutos,
+	horas,
+	dias,
+	resetearContador,
+	paraContador,
+
+}) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<Contador
+				segundos={segundos}
+				minutos={minutos}
+				horas={horas}
+				dias={dias}
+				resetearContador={resetearContador}
+			/>
+		</>
 	);
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,46 +1,52 @@
 //import react into the bundle
-
 import React from "react";
 import ReactDOM from "react-dom";
-import PropTypes from "prop-types";
-
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-function SimpleCounter(props){
-    return(<div className="bigCounter">
-        <div className="calendar">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock-fill" viewBox="0 0 16 16">
-  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
-</svg>
-        </div>
-  
-           <div className="four">{props.digitFour %10}</div>
-           <div className="three">{props.digitThree %10}</div>
-           <div className="two">{props.digitTwo %10}</div>
-           <div className="one">{props.digitOne % 10}</div>       
-    </div>);
+//import your own components
+import Home from "./component/home.jsx";
+
+let segundos = 0;
+let minutos = 0;
+let horas = 0;
+let dias = 0;
+
+const resetearContador = () => {
+	segundos = -1;
+	minutos = 0;
+	horas = 0;
+	dias = 0;
 };
 
-SimpleCounter.protoType = {
-    digitFour: PropTypes.number,
-    digitThree: PropTypes.number,
-    dogitTwo: PropTypes.number,
-    digitOne: PropTypes.number,
-};
-  
-let counter =0;
-setInterval(function(){
-    const four = Math.floor(counter/1000);
-    const three = Math.floor(counter/100);
-    const two = Math.floor(counter/10);
-    const one = Math.floor(counter/1);
-    console.log(four, three, two, one);
-//render your react application
-  counter++;
- ReactDOM.render(
-    <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />,
-     document.querySelector("#app")
- );
-},1000);
+
+
+
+const incrementar = setInterval(() => {
+	segundos++;
+	if (segundos === 60) {
+		minutos++;
+		segundos = 0;
+	}
+	if (minutos === 60) {
+		horas++;
+		minutos = 0;
+	}
+	if (horas === 24) {
+		dias++;
+		horas = 0;
+	}
+
+	//render your react application
+	ReactDOM.render(
+		<Home
+			segundos={segundos}
+			minutos={minutos}
+			horas={horas}
+			dias={dias}
+			resetearContador={resetearContador}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
